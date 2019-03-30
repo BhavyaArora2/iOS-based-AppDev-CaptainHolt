6e0abb67811bf97e3c999fd72035c176f05bc7b6
Bhavya Arora <33778699+BhavyaArora2@users.noreply.github.com>
2019-03-30 18:13:00 +0530

    Add files via upload
    iOS AppDevelopment

---
+++ iOS development Captain Holt Sound Board/iOS development Captain Holt Sound Board/HoltTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HoltTableViewCell.swift
+//  iOS development Captain Holt Sound Board
+//
+//  Created by ADG RIT 1 on 30/03/19.
+//  Copyright © 2019 ADG RIT 1. All rights reserved.
+//
+
+import UIKit
+
+class HoltTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
